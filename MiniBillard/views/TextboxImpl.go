04422dd1d64b265d10402c6432d2d6e3c3a071dc
Fussbroich <127486905+Fussbroich@users.noreply.github.com>
2024-04-10 09:40:02 +0200

    added text wrapping

--- MiniBillard/views/TextboxImpl.go
+++ MiniBillard/views/TextboxImpl.go
@@ -2,8 +2,12 @@ package views
 
 import (
 	"gfx"
+	"math"
+	"unicode"
+	"unicode/utf8"
 )
 
+// Eine simple Box, die Text in Zeilen umbricht und anzeigt.
 type textbox struct {
 	text string
 	widget
@@ -14,13 +18,65 @@ func NewTextFenster(startx, starty, stopx, stopy uint16, t string, hg, vg Farbe,
 	return &textbox{text: t, widget: fenster}
 }
 
+func worteInZeilen(worte []string, lMax int) (zeilen []string) {
+	var zeile string
+	for _, wort := range worte {
+		if utf8.RuneCountInString(wort) > lMax {
+			zeilen = append(zeilen, wort)
+			continue
+		}
+		if (utf8.RuneCountInString(zeile) + utf8.RuneCountInString(wort) + 1) <= lMax {
+			if zeile != "" {
+				zeile += " "
+			}
+			zeile += wort
+		} else {
+			zeilen = append(zeilen, zeile)
+			zeile = wort
+		}
+	}
+	if zeile != "" {
+		zeilen = append(zeilen, zeile)
+	}
+	return
+}
+
+func textInWorte(text string) (worte []string) {
+	var wort string
+	// Zerlege den Text in Worte.
+	for _, char := range text {
+		if unicode.IsSpace(char) {
+			if wort != "" {
+				worte = append(worte, wort)
+				wort = ""
+			}
+		} else {
+			wort += string(char)
+		}
+	}
+	if wort != "" {
+		worte = append(worte, wort)
+	}
+	return
+}
+
 func (f *textbox) Zeichne() {
 	f.widget.Zeichne()
-	//breite, höhe := f.GibGröße()
+	B, H := f.GibGröße()
 	fp := fontDateipfad("LiberationMono-Regular.ttf")
 	r, g, b := f.vg.RGB()
 	gfx.Stiftfarbe(r, g, b)
-	schriftgröße := 24
-	gfx.SetzeFont(fp, schriftgröße)
-	gfx.SchreibeFont(f.startX+f.eckradius, f.startY+f.eckradius, f.text)
+	// Schriftgröße automatisch anpasssen bzgl. Gesamtfläche der Box
+	var sh, sb int
+	sh = int(math.Min(24, math.Sqrt(float64(B*H)/float64(utf8.RuneCountInString(f.text))*12/7*5/6)))
+	println("berechnet:", sh)
+	sb = 7 * sh / 12
+	gfx.SetzeFont(fp, sh)
+	zMax, cMax := int(H)/sh, int(B)/sb
+	for z, zeile := range worteInZeilen(textInWorte(f.text), cMax) {
+		if z > zMax {
+			break
+		}
+		gfx.SchreibeFont(f.startX+f.eckradius, f.startY+f.eckradius+uint16(z*(sh*6/5)), zeile)
+	}
 }
