bb3e50c80de812ce17b9181c60974ae5ab9fe544
Fussbroich <127486905+Fussbroich@users.noreply.github.com>
2024-04-23 18:58:59 +0200

    minor maintenance

--- MiniBillard/views_controls/EingabeRoutinenImpl.go
+++ MiniBillard/views_controls/EingabeRoutinenImpl.go
@@ -22,22 +22,19 @@ func abfangenFensterZu() {
 }
 
 func NewMausRoutine(f func(t uint8, s int8, x uint16, y uint16)) *bpEingabeRoutine {
-	ctl := bpEingabeRoutine{}
-	rfunc := func() {
-		if !gfx.FensterOffen() {
-			return
-		}
-		//defer abfangenFensterZu() // TODO: testen und einbauen
-		taste, status, mausX, mausY := gfx.MausLesen1() // blockiert, bis Maus bedient
-		f(taste, status, mausX, mausY)
-	}
-	ctl.Routine = hilf.NewRoutine("Maussteuerung", rfunc)
-	return &ctl
+	return &bpEingabeRoutine{Routine: hilf.NewRoutine("Maussteuerung",
+		func() {
+			if !gfx.FensterOffen() {
+				return
+			}
+			//defer abfangenFensterZu() // TODO: testen und einbauen
+			taste, status, mausX, mausY := gfx.MausLesen1() // blockiert, bis Maus bedient
+			f(taste, status, mausX, mausY)
+		})}
 }
 
 func NewTastenRoutine(f func(uint16, uint8, uint16)) *bpEingabeRoutine {
-	ctl := bpEingabeRoutine{}
-	rfunc :=
+	return &bpEingabeRoutine{Routine: hilf.NewRoutine("Tastensteuerung",
 		func() {
 			if !gfx.FensterOffen() {
 				return
@@ -45,7 +42,5 @@ func NewTastenRoutine(f func(uint16, uint8, uint16)) *bpEingabeRoutine {
 			//defer abfangenFensterZu() // TODO: testen und einbauen
 			taste, gedrückt, tiefe := gfx.TastaturLesen1() // blockiert, bis Taste gedrückt
 			f(taste, gedrückt, tiefe)
-		}
-	ctl.Routine = hilf.NewRoutine("Maussteuerung", rfunc)
-	return &ctl
+		})}
 }
