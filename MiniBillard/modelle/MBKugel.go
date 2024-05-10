40f79abf03210392c8a6140317e770d92c94d9d9
Fussbroich <127486905+Fussbroich@users.noreply.github.com>
2024-05-10 11:57:15 +0200

    maintained some comments

--- MiniBillard/modelle/MBKugel.go
+++ MiniBillard/modelle/MBKugel.go
@@ -3,7 +3,7 @@ package modelle
 import "../hilf"
 
 // Eine MBKugel repräsentiert eine Billardkugel in 2 Dimensionen. Sie hat eine Position
-// (Mittelpunktskoordinaten), einen Radius, eine Bewegungs-Geschwindigkeit.
+// (Ortsvektor der Mittelpunktskoordinaten), einen Radius, eine Bewegungs-Geschwindigkeit.
 // Außerdem kennt sie ihre Nummer (Wert) im Billardspiel.
 //
 //	NewKugel(pos hilf.Vec2, r float64, wert uint8) erzeugt ein Objekt
@@ -18,26 +18,37 @@ type MBKugel interface {
 	//	Hinweis: Bei der derzeitigen Implementierung wird die eigentliche Bewegungsgeschwindigkeit durch
 	//	erhöhen oder vermindern der Aufruf-Frequenz beeinflusst.
 	BewegenIn(MiniBillardSpiel)
+
 	// Notifier einer anderen Kugel über die Kollision mit ihr.
 	SetzeKollidiertMit(MBKugel)
+
 	// Löscht Nachricht über die Kollision mit anderer Kugel.
 	SetzeKollidiertZurueck()
+
 	// Getter, ob in einer Tasche.
 	IstEingelocht() bool
+
 	// Getter für die vektorielle (2D) Bewegungsgeschwindigkeit.
 	GibV() hilf.Vec2
-	// Getter für die vektorielle (2D) Bewegungsgeschwindigkeit.
+
+	// Setter für die vektorielle (2D) Bewegungsgeschwindigkeit.
 	SetzeV(hilf.Vec2)
+
 	// Setze die vektorielle (2D) Bewegungsgeschwindigkeit auf Null.
 	Stop()
-	// Getter für die vektorielle (2D) Position auf dem Tuch.
+
+	// Getter für die vektorielle (2D) Position ("Ortsvektor") auf dem Tuch.
 	GibPos() hilf.Vec2
-	// Setter für die vektorielle (2D) Position auf dem Tuch.
+
+	// Setter für die vektorielle (2D) Position ("Ortsvektor") auf dem Tuch.
 	SetzePos(hilf.Vec2)
+
 	// Getter für den in der Simulation gedachten Radius in Pixeln.
 	GibRadius() float64
+
 	// Getter für die Nummer (Wert) der Kugel
 	GibWert() uint8
+
 	// Getter für eine komplett unabhängige Kopie derselben Kugel
 	GibKopie() MBKugel
 }
--- MiniBillard/modelle/MBSpiel.go
+++ MiniBillard/modelle/MBSpiel.go
@@ -27,26 +27,37 @@ type MiniBillardSpiel interface {
 	//	Eff.: Jede enthaltene Kugel wird in konstanten Abständen bewegt;
 	//	die Spielregeln, sowie der Kugelstillstand werden laufen überwacht.
 	Starte()
+
 	// Stoppt die Spielschleife (Pause-Modus) - lässt sich wieder starten.
 	//	Vor.: Spielschleife läuft
 	//	Eff.: Spielschleife läuft nicht
 	Stoppe()
+
+	// Stelle fest, ob Spielschleife läuft.
 	Laeuft() bool
+
 	// Verringert den Betrag der Kugelbewegungen um einen festen Quotienten
 	//	Vor.: keine
 	//	Eff.: Kugeln bewegen sich langsamer
 	ZeitlupeAnAus()
+
+	// Friere Spielschleife ein oder taue sie wieder auf.
 	PauseAnAus()
+
+	// Stelle fest, ob Zeitlupe aktiv ist.
 	IstZeitlupe() bool
+
 	// Nur wenn alle Kugeln stillstehen, kann die weiße Spielkugel angestossen werden.
 	//
 	//	Vor.: Alle Kugeln stehen still.
 	//	Eff.: Die Spielkugel bewegt sich mit einer vorher gesetzten Geschwindigkeit und Richtung.
 	Stosse()
+
 	// Alle Kugeln werden in den Zustand vor dem vergangenen Stoß versetzt.
 	//
 	//	Eff.: Kugeln stehen still auf den Positionen vor dem vergangenen Stoß.
 	StossWiederholen()
+
 	// Das ganze Spiel wird neu begonnen.
 	//
 	//	Eff.: Alle Kugeln wie zu Beginn des Spiels, Spielkugel in der Küche an einer
@@ -54,44 +65,63 @@ type MiniBillardSpiel interface {
 	Reset()
 	IstStillstand() bool
 	GibTaschen() []MBTasche
+
 	// Getter für alle Kugeln, die im Spiel sind.
 	GibKugeln() []MBKugel
+
 	// Getter nur für diejenigen Kugeln, die noch nicht eingelocht sind.
 	GibAktiveKugeln() []MBKugel
+
 	// Notifier für die Berührung zwischen zwei Kugeln.
 	NotiereBerührt(MBKugel, MBKugel)
+
 	// Notifier für das Einlochen einer Kugel.
 	NotiereEingelocht(MBKugel)
+
 	// Getter nur für diejenigen Kugeln, die bereits eingelocht sind in der Reihenfolge des Einlochens.
 	GibEingelochteKugeln() []MBKugel
+
 	// Getter nur für die weiße Spielkugel.
 	GibSpielkugel() MBKugel
+
 	// Setter für bestimmte Kugelkonstellationen
 	SetzeKugeln1BallTest() // Testzwecke
 	SetzeKugeln3Ball()     // Testzwecke
 	SetzeKugeln9Ball()     // Testzwecke
+
 	// Getter für die aktuell gesetzte vektorielle (2D) Stoßgeschwindigkeit (Betrag und Richtung).
 	GibVStoss() hilf.Vec2
+
 	// Setter für die vektorielle (2D) Stoßrichtung.
 	SetzeStossRichtung(hilf.Vec2)
+
 	// Setter für den Betrag der Stoßkraft.
 	SetzeStosskraft(float64)
+
 	// Setter für die gesamte Zeitdauer zum "Spiel gegen die Zeit".
 	SetzeSpielzeit(time.Duration)
+
 	// Setter für die künstliche Beeinflussung der verbleibenden Spieldauer.
 	SetzeRestzeit(time.Duration) // Testzwecke
+
 	// Getter für die verbleibende Spieldauer.
 	GibRestzeit() time.Duration
+
 	// Getter für die Anzahl der in dem Spiel bereits versenkten (eingelochten) Kugeln
 	GibTreffer() uint8
+
 	// Getter für die Anzahl der Fouls in diesem Spiel.
 	GibStrafpunkte() uint8
+
 	// Prüft, ob alle Kugeln, bis auf die weiße Spielgkugel, bereits eingelocht sind.
 	AlleEingelocht() bool
+
 	// Reduziere künstlich die Anzahl der Fouls in diesem Spiel um 1.
 	ReduziereStrafpunkte()
+
 	// Erhöhe künstlich die Anzahl der Fouls in diesem Spiel um 1.
 	ErhoeheStrafpunkte() // Testzwecke
+
 	// Getter für die gedachte Pixelgröße der Simulation.
 	GibGroesse() (float64, float64)
 }
